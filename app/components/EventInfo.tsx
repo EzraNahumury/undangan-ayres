46de03495b2c72e76f261f120fe42d9d4158dfd0
Ezra Kristanto Nahumury <117893407+EzraNahumury@users.noreply.github.com>
2026-04-27 13:22:08 +0700

    add card

--- app/components/EventInfo.tsx
+++ app/components/EventInfo.tsx
@@ -27,6 +27,15 @@ const details = [
     label: "Location",
     value: "Ayres Apparel Solo, Ruko Menco City",
   },
+  {
+    icon: (
+      <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 14.25l6-6m4.5-3.493V21.75l-3.75-1.5-3.75 1.5-3.75-1.5-3.75 1.5V4.757c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0c1.1.128 1.907 1.077 1.907 2.185zM9.75 9h.008v.008H9.75V9zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm4.125 6h.008v.008h-.008V15zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+      </svg>
+    ),
+    label: "Voucher",
+    value: "Kupon Diskon 20%",
+  },
 ];
 
 export default function EventInfo() {
